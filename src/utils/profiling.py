01b67a0435d90edad10d0038ff5a2ba7aeab6877
Yoruko Tang <mt350@duke.edu>
2024-01-30 15:39:05 -0500

    fvcore

--- src/utils/profiling.py
+++ src/utils/profiling.py
@@ -1,21 +1,11 @@
-
-from fvcore.nn import FlopCountAnalysis,flop_count_table
+from models import get_net
+from fvcore.nn import FlopCountAnalysis,flop_count_table, parameter_count
 import torch
-import argparse
-import models
-parser = argparse.ArgumentParser()
-parser.add_argument('--gpu', default=None, 
-                    help="To use cuda, set to a specific GPU ID. Default set to use CPU.")
 
-args = parser.parse_args()
-device = torch.device('cuda:{}'.format(args.gpu) if args.gpu is not None else 'cpu')
 
-# model = models.get_net('vgg11', 'cifar', False, 10).to(device)
-model = models.LeNet5().to(device)
-# x = torch.rand([10,3,32,32]).to(device)
-# 10 3 32 32 
-# model = VGG11(block=0).to(device)
-x = torch.rand([10,1,28,28]).to(device)
+model = get_net('resnet50','cifar',num_classes=10)
 print(model)
+x = torch.rand([10,3,32,32])
 flops = FlopCountAnalysis(model,x)
-print(flop_count_table(flops))
+print(flops.by_module())
+print(parameter_count(model))
