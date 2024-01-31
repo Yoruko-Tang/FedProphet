2eccecef78e6727aace83e34ed32c67fc22bae40
easonwang717 <easonwang717@gmail.com>
2024-01-31 17:09:51 -0500

    model profiliing

--- src/utils/profiling.py
+++ src/utils/profiling.py
@@ -3,34 +3,34 @@ from fvcore.nn import FlopCountAnalysis,flop_count_table, parameter_count
 import torch
 
 
-model = get_net('resnet50','cifar',num_classes=10)
+# model = get_net('vgg11','cifar',num_classes=10)
 # print("model-----------------------------------------")
-print(model)
-# print("----------------------------------------------")
+# print(model)
+# # print("----------------------------------------------")
 
-x = torch.rand([10,3,32,32])
-#flops = FlopCountAnalysis(model,x)
+# x = torch.rand([10,3,32,32])
+# flops = FlopCountAnalysis(model,x)
 
-print("flops per module----------------------------- ")
-#flops_per_module = flops.by_module()
-flops_per_module = parameter_count(model)
-sorted_flops_per_module = {}
-sum = 0
-dot_count = 0
-for key in flops_per_module.keys():
-    for c in key:
-        if c == '.':
-            dot_count += 1
-    if (dot_count == 0 and 'layer' not in key and 'classifier' not in key and 'features' not in key) \
-        or dot_count == 1:
-        sorted_flops_per_module[key] = flops_per_module[key]
-    dot_count = 0
+# print("flops per module----------------------------- ")
+# flops_per_module = flops.by_module()
+# #flops_per_module = parameter_count(model)
+# sorted_flops_per_module = {}
+# sum = 0
+# dot_count = 0
+# for key in flops_per_module.keys():
+#     for c in key:
+#         if c == '.':
+#             dot_count += 1
+#     if (dot_count == 0 and 'layer' not in key and 'classifier' not in key and 'features' not in key) \
+#         or (dot_count == 1 and 'weight' not in key and 'bias' not in key):
+#         sorted_flops_per_module[key] = flops_per_module[key]
+#     dot_count = 0
 
-print(sorted_flops_per_module)
-for key in sorted_flops_per_module:
-    if key != '':
-        sum += sorted_flops_per_module[key]
-print(sum-9408-128-20490)
+# print(sorted_flops_per_module)
+# for key in sorted_flops_per_module:
+#     if key != '':
+#         sum += sorted_flops_per_module[key]
+# print(sum)
 
 # temp = 0
 # print(sorted_flops_per_module)
@@ -54,3 +54,52 @@ print(sum-9408-128-20490)
 # print("params per module-----------------------------")
 # print(parameter_count(model))
 # print("----------------------------------------------")
+
+def profile_model(model, inputsize):
+    x = torch.rand(inputsize)
+
+    flops = FlopCountAnalysis(model,x)
+    _flops_per_module = flops.by_module()
+    _params_per_module = parameter_count(model)
+    flops_per_module = {}
+    params_per_module = {}
+    dot_count = 0
+    for key in _flops_per_module.keys():
+        for c in key:
+            if c == '.':
+                dot_count += 1
+        if (dot_count == 0 and 'layer' not in key and 'classifier' not in key and 'features' not in key) \
+            or (dot_count == 1 and 'weight' not in key and 'bias' not in key):
+            flops_per_module[key] = _flops_per_module[key]
+            params_per_module[key] = _params_per_module[key]
+        dot_count = 0
+
+    flops_per_module['total'] = flops_per_module.pop('')
+    params_per_module['total'] = params_per_module.pop('')
+
+    def validate(dic):
+        sum = 0
+        for key in dic:
+            if key != 'total':
+                sum += dic[key]
+        assert sum == dic['total'], "Wrong profiling data!"
+    
+    validate(flops_per_module)
+    validate(params_per_module)
+
+    print(flops_per_module)
+    print(params_per_module)
+
+    print(len(flops_per_module))
+    print(len(params_per_module))
+    
+
+
+model = get_net('vgg16','cifar',num_classes=10)
+inputsize = [10,3,32,32]
+profile_model(model,inputsize)
+
+
+    
+
+    
