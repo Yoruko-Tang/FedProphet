0bb29d0a68999418f29fee9e3ab3acfa2dff2fe4
Yoruko Tang <mt350@duke.edu>
2024-05-07 11:21:53 -0400

    modify mem calculation for kd and pt

--- src/scheduler/pt_scheduler.py
+++ src/scheduler/pt_scheduler.py
@@ -17,9 +17,9 @@ class pt_scheduler(base_AT_scheduler):
         args = super().training_params()
 
         avail_mem = self.available_memory[idx]
-        if args["adv_train"]:
-            # reserve memory for adversarial training
-            avail_mem -= np.prod(self.model_profile.inputsize)*self.model_profile.data_Byte*args["adv_ratio"]
+        # if args["adv_train"]:
+        #     # reserve memory for adversarial training
+        #     avail_mem -= np.prod(self.model_profile.inputsize)*self.model_profile.data_Byte*args["adv_ratio"]
         frac = min([avail_mem/self.memory_req,1.0])
         neuron_dict = {m:None for m in self.neuron_num}
         
